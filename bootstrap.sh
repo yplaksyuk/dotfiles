ef250f27bad075ebc58dd30348ea2984d27af071
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2019-10-04 11:46:13 +0300

    added 'git-cleanup'

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-DOTFILES=`dirname $0`
+cd `dirname "$0"`
+DOTFILES=`pwd -P`
+cd -
+
+mkdir -p ~/.local/bin
+
+ln -fs ${DOTFILES}/bin/git-cleanup ~/.local/bin/git-cleanup
 
 ln -fs ${DOTFILES}/vimrc ~/.vimrc
 ln -fs ${DOTFILES}/vim ~/.vim
